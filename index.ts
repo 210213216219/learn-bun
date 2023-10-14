39aaae0b3fdbbed091816ca80889dbd8ecd6e51f
xllyllz <82016137+210213216219@users.noreply.github.com>
2023-10-14 14:52:03 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ export default {
   fetch () {
     let headers = new Headers();
     headers.append("Content-type", "text/html; charset=utf-8");
-    return new Response(`<title>learn-bun</title><h1>hello world</h1>`, { headers: headers })
+    return new Response(`<style></style><title>learn-bun</title><h1>hello world</h1>`, { headers: headers })
   }
 }
